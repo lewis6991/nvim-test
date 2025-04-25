bd84437fd1fd399fcf6acf4b7009cd7c76b11e80
Lewis Russell <lewis6991@gmail.com>
2025-04-25 13:46:38 +0100

    fixup!

--- lua/busted/modules/output_handler_loader.lua
+++ lua/busted/modules/output_handler_loader.lua
@@ -1,5 +1,4 @@
 local path = require('pl.path')
-local hasMoon, moonscript = pcall(require, 'moonscript')
 local utils = require('busted.utils')
 
 return function()
@@ -10,8 +9,6 @@ return function()
     local success, err = pcall(function()
       if output:match('%.lua$') then
         handler = dofile(path.normpath(output))
-      elseif hasMoon and output:match('%.moon$') then
-        handler = moonscript.dofile(path.normpath(output))
       else
         handler = require('busted.outputHandlers.' .. output)
       end
